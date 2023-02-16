912d52d474d225ed01c21cf8f8acba1e5fc3be11
kg191968 <bennis.mehdi@courrier.uqam.ca>
2023-02-15 20:14:20 -0500

    "reinitialized commit"

--- src/LectureFichier.java
+++ src/LectureFichier.java
@@ -3,17 +3,27 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.lang.*;
+
+
 public class LectureFichier {
-    public static int Base ;
-    public static int Largeur;
     public static void LectureFichier(String[] args) {
         try {
             File Fichier = new File("nomfichier.txt");
             Scanner Lecteur = new Scanner(Fichier);
-            ArrayList<String> TabDimensions = new ArrayList<String>();
+            int nbLignes = 0;
+            while(Lecteur.hasNextLine()) {
+                Lecteur.nextLine();
+                nbLignes++;
+            }
+            int[][] TabDimensions = new int[nbLignes][];
+            int i = 0;
+            String[] ValeursLigne = new String[2];
             while (Lecteur.hasNextLine()){
                 String Ligne = Lecteur.nextLine();
-                TabDimensions.append(Integer.parseInt(Ligne.split(" ")));
+                ValeursLigne = Ligne.split(" ");
+                TabDimensions[i][0] = Integer.parseInt(ValeursLigne[0]);
+                TabDimensions[i][1] = Integer.parseInt(ValeursLigne[1]);
+                i += 1;
             }
             Lecteur.close();
         } catch (FileNotFoundException e) {
