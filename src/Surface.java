eeb47a0c5c3e4d335b57cb6ab38ebf55d4f8919d
kg191968 <bennis.mehdi@courrier.uqam.ca>
2023-02-17 21:34:03 -0500

    "reinitialized commit"

--- src/Surface.java
+++ src/Surface.java
@@ -1,15 +1,28 @@
 import java.util.ArrayList;
 public class Surface {
     public static LectureFichier Dimensions;
-    public static ArrayList<int[]> ListeDimensions;
-    public static int Base ;
-    public static int Largeur;
-    public static void Surface (){
-        ListeDimensions = Dimensions.retournerListeDimensions();
-        int i;
-        for(i = 0; i < ListeDimensions.toArray().length; i++){
-            ListeDimensions[i][0] = new Base;
-            ListeDimensions[i][1] = new Largeur;
+    public static ArrayList<int[]> ListeDimensions = Dimensions.retournerListeDimensions();
+    public static int i;
+    public static int[] surfacePiece = ListeDimensions.get(i);
+    public static int Base = surfacePiece[0];
+    public static int Largeur = surfacePiece[1];
+
+    public void setDimensions(){
+        Base = this.Base;
+        Largeur = this.Largeur;
+    }
+
+    public static void printO(int Nombre){
+        System.out.print("O");
+    }
+
+    public static void Surface(int Base, int Largeur){
+        int x;
+        int y;
+        for(x = 0; x < Base; x++){
+            for(y = 0; y < Largeur; y++){
+                printO(Largeur);
+            }
         }
     }
 }
